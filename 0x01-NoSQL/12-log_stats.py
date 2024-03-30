da5c1a5b886f956211314481bbd5b17eaf6bc729
KOBBY_IM <nyarkotheo2@gmail.com>
2024-03-30 21:10:47 +0000

    task 12 and 13 code fixed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,38 +1,21 @@
 #!/usr/bin/env python3
 """ log stats module """
 
-import pymongo
 
-# Create a connection to the MongoDB server
-connection = pymongo.MongoClient("mongodb://localhost:27017/")
+from pymongo import MongoClient
 
-# Select the database
-db = connection.logs
+if __name__ == "__main__":
+    client = MongoClient()
+    logs = client.logs.nginx
 
-# Select the collection
-collection = db.nginx
+    n_docs = logs.count_documents({})
+    print(f"{n_docs} logs")
 
+    print("Methods:")
 
-# Get the number of documents in the collection
-number_of_logs = collection.count_documents({})
-print(f"{number_of_logs} logs")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        n_count = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {n_count}")
 
-# Create the queries
-queries = [{'method': 'GET'}, {'method': 'POST'}, {'method': 'PUT'},
-           {'method': 'PATCH'}, {'method': 'DELETE'}, {'path': '/status'}]
-
-# Initialize an empty list to store the results
-results = []
-
-# Iterate through the queries
-for query in queries:
-    # Get the number of documents that match the query
-    number_of_documents = collection.count_documents(query)
-    # Add the result to the list
-    results.append(number_of_documents)
-
-# Print the results
-print("Methods:")
-for i, result in enumerate(results):
-    print(f"\tmethod {['GET', 'POST', 'PUT', 'PATCH', 'DELETE',
-                       'status check'][i]}: {result}")
+    n_count = logs.count_documents({"method": "GET", "path": "/status"})
+    print(f"{n_count} status check")
